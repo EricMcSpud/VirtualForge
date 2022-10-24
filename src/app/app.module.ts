b6c7125e055f3c243a651fd8e9440fb121eabf56
EricJDaws <eric.daws@screenmedia.co.uk>
2022-10-24 22:59:27 +0100

    Incremental commit 1.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './common/header/header.component';
+import { FooterComponent } from './common/footer/footer.component';
+import { ProductListComponent } from './shopping/product-list/product-list.component';
+import { ProductSummaryComponent } from './shopping/product-summary/product-summary.component';
+import { ProductCartComponent } from './shopping/product-cart/product-cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ProductListComponent,
+    ProductSummaryComponent,
+    ProductCartComponent
   ],
   imports: [
     BrowserModule,
